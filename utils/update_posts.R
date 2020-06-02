80cce9fc9862848be779781f5106fbf8f3e2d8be
Marcus Carvalho <marcuswac@gmail.com>
2020-06-01 21:05:13 -0300

    adding initial post

---
+++ utils/update_posts.R
@@ -0,0 +1,21 @@
+require(stringr)
+require(magrittr)
+require(future)
+require(furrr)
+require(purrr)
+
+posts <- c("_posts/global" , list.dirs("_posts/national", recursive = FALSE))
+
+
+future::plan("multisession")
+
+safe_render <- purrr::safely(rmarkdown::render)
+
+rendered_output <- furrr::future_map(posts, 
+            function(post) {
+              post_name <- stringr::str_split(post, "/")[[1]] %>% 
+                dplyr::last()
+              file.copy("library.bib", file.path(post), overwrite = TRUE)
+              
+              safe_render(file.path(post, paste0(post_name, ".Rmd")), quiet = FALSE)
+            }, .progress = TRUE)
