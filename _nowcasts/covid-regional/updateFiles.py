6c4ea1ee79649f4ec71bf9513cac9e011c1a0d92
André Felipe Oliveira de Azevedo Dantas <lordcobisco@hotmail.com>
2020-06-03 19:27:06 -0300

    Update updateFiles.py

--- _nowcasts/covid-regional/updateFiles.py
+++ _nowcasts/covid-regional/updateFiles.py
@@ -10,4 +10,4 @@ def copiarPastas(src,dest):
 os.system('cd /home/travis/build/ \n  git clone https://neurodeveloperISD:,Lc258147@github.com/C4NESub9/googleData.git')
 copiarPastas('/home/travis/build/lordcobisco/covid-br-model-epiforecasts/_nowcasts/covid-regional/brazil/cities/','/home/travis/build/googleData/dataMC/cities/')
 copiarPastas('/home/travis/build/lordcobisco/covid-br-model-epiforecasts/_nowcasts/covid-regional/brazil/cities-summary/','/home/travis/build/googleData/dataMC/cities-summary/')
-os.system('cd /home/travis/build/googleData/ \n git remote set-url origin https://neurodeveloperISD:,Lc258147@github.com/googleData.git \n git add . \n git commit -m "Atualizacao dos dados de Salvador" \n git push')
+os.system('cd /home/travis/build/googleData/ \n git remote set-url origin https://neurodeveloperISD:,Lc258147@github.com/C4NESub9/googleData.git \n git add . \n git commit -m "Atualizacao dos dados de Salvador" \n git push')
